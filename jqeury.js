8d131b2f87b4d9f0f305ef1f53a0eb0f515a7a3d
park geon do <wksehs123@naver.com>
2022-11-29 21:45:12 +0900

    first commit

--- jqeury.js
+++ jqeury.js
@@ -1,5 +1,26 @@
+let Constellations = [
+    {first:{x:10,y:10},second:{x:10,y:10},third:[50,30]},
+    {first:{x:10,y:10},second:{x:10,y:10},third:{x:10,y:10}},
+    {first:{x:10,y:10},second:{x:10,y:10},third:{x:10,y:10}}
+]
+
+
 $(function () {
     $(document).on("click", function() {
     $('.preview').slideUp();
     })
+
+    let designer = $('<div class="box"></div>');
+    for(let i = 0; i < 3; ++i){
+        console.log("as")
+        designer.append('<div class = "star"/>')
+        $('.Constellation').append(designer);
+        $(`.star:nth-child(${i+1})`).css({'transform':'translate(50px,50px)'})
+        // $('.Constellation').append(designer);
+        // $('.star:nth-child(1)').css('transform','translate(50px,50px)')
+        console.log(Constellations[i].first.x)
+    }
 })
+
+
+
--- style.js
+++ style.js
@@ -1,21 +1,48 @@
-let hover = false
+let hover = false;
+let click = false;
 
 $(function () {
-let star1 = document.querySelector('#star');
-      let light = document.querySelector('#light');
+    show()
+    let star1 = document.querySelector('#star');
+    let light = document.querySelector('#light');
+    let back = document.querySelector('.back');
       
-      star1.addEventListener('mouseenter',function(){
+    star1.addEventListener('mouseenter',function(){
         light.setAttribute("animation-mixer","clip:connection")
         light.setAttribute("animation-mixer","loop:once")
         star1.setAttribute("animation","dur:100000000")
-      })
-      star1.addEventListener('mouseleave',function(){
+        hover = true;
+        show();
+    })
+    star1.addEventListener('mouseleave',function(){
         star1.setAttribute("animation","dur:250000;")
-      })
+        hover = false;
+        show();
+    })
+    star1.addEventListener('click',function(){
+        click = true;
+        modal();
+    })
+    back.addEventListener('click',function(){
+        click = false;
+        modal();
+    })
 })
 
 function show(){
     if(hover){
-        $('.preview').slideUp();
+        $('.preview').stop(true).delay(200).slideDown();
+    }else{
+        $('.preview').stop(true).slideUp();
+    }
+}
+
+function modal(){
+    if(click){
+        $('.modal').fadeIn(100)
+        $('.background').fadeIn(100)
+    }else{
+        $('.modal').fadeOut(100)
+        $('.background').fadeOut(100)
     }
 }
